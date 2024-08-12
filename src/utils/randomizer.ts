d09c3964ab7e664b57a29b6f2777bf4d8d8a1bb9
Francesco Di Pietro <71017054+francesco892@users.noreply.github.com>
2024-08-12 16:22:25 +0200

    [UPDATE] randomization + darker shade of gray on current-color

--- src/utils/randomizer.ts
+++ src/utils/randomizer.ts
@@ -6,7 +6,8 @@ export function getRandomElements<T>(array: T[], nElements: number): T[] {
     return shuffleArray(array).slice(0, nElements);
 }
 
-export function shuffleArray<T>(array: T[]): T[] {
+export function shuffleArray<T>(arrayToShuffle: T[]): T[] {
+    const array = [...arrayToShuffle];
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
